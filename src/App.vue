6f3e29cf7161b0c640e61158530cba8c2f96c962
MohsinMalik2 <malikmuhsin70@gmail.com>
2022-03-15 13:01:37 +0500

    simple vue calculator

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<HeadingComponent/>
+  <CalculatorComponent msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import CalculatorComponent from './components/CalculatorComponent.vue'
+import HeadingComponent from './components/Heading.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorComponent,
+    HeadingComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  position: absolute;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  left:35%;
+  top:20%;
+}
+</style>
---
+++ src/components/Heading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-center">
+        <h1>
+          Calculator
+        </h1>
+        <p>
+            Calculate the Infinite with BuggBear.
+        </p>
+  </div>
+</template>
+
+<script>
+export default {
+name:"HeadingComponent"
+}
+</script>
+
+<style>
+.text-center{
+    text-align: center;
+}
+</style>
